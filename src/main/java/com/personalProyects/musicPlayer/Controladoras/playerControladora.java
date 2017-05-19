f98bbb421207dd7e10bbdd23c29067315dc66895
Manuel Tolentino <manutm24@gmail.com>
2017-05-19 18:41:39 -0400

    SEGUNDO COMMIT (CHEQUEAR CLASE PLAYERCONTROLADORA)

--- src/main/java/com/personalProyects/musicPlayer/Controladoras/playerControladora.java
+++ src/main/java/com/personalProyects/musicPlayer/Controladoras/playerControladora.java
@@ -2,9 +2,14 @@ package com.personalProyects.musicPlayer.Controladoras;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by mt on 19/05/17.
@@ -12,6 +17,11 @@ import java.util.ArrayList;
 public class playerControladora {
     //Variables Generales
     String ubicacionCarpeta = "/home/mt/IdeaProjects/musicPLayer/src/main/resources/Musica/";
+    private MediaPlayer mp;
+    public Media me;
+    Random numeroRandom = new Random();
+    int max=leerArchivosMP3Carpeta(ubicacionCarpeta).size();
+    int min=1;
 
 
     //Botones
@@ -24,6 +34,12 @@ public class playerControladora {
         if(archivosLeidos.isEmpty()){
             System.out.println("Carpeta Vacia");
         }
+    }
+
+    @FXML
+    void play(ActionEvent event) {
+        System.out.println(max);
+        mp3(null).play();
     }
 
     //Funciones
@@ -41,4 +57,36 @@ public class playerControladora {
         }
         return listaCanciones;
     }
+
+    public MediaPlayer mp3(String cancionSeleccionada){
+        int proxCancion = numeroRandom.nextInt(max)+ min;
+        String nombreCancion = leerArchivosMP3Carpeta(ubicacionCarpeta).get(proxCancion).getName();
+        String archivo;
+        if(cancionSeleccionada!=null){ //GENERA LA CANCION A PARTIR DE LA SELECCIONADA EN EL LISTVIEW
+            archivo = new File(ubicacionCarpeta + cancionSeleccionada).getAbsolutePath();
+        }
+        else{ //NO HAY CANCIONES SELECCIONADAS, GENERA UNA CANCION RANDOM
+            archivo = new File(ubicacionCarpeta + nombreCancion ).getAbsolutePath();
+            System.out.println(archivo.isEmpty());
+        }
+
+
+
+        try{
+//ERROR!!!!!!!!!!!!!!!!!!!!!!!!!
+            me = new Media(new File(archivo).toURI().toString());
+            mp = new MediaPlayer(me);
+
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+        return mp;
+    }
+
+
+
+
+
 }
