50883dacd228254779de31328ff47532eda020c7
MT <manutm24@gmail.com>
2017-05-20 12:29:32 -0400

    LISTVIEW CANCIONES Y LISTVIEW ALBUMS, (CHEQUIAR FUNCION PLAY)

--- src/main/java/com/personalProyects/musicPlayer/Controladoras/playerControladora.java
+++ src/main/java/com/personalProyects/musicPlayer/Controladoras/playerControladora.java
@@ -1,9 +1,13 @@
 package com.personalProyects.musicPlayer.Controladoras;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+
 
 
 import java.io.File;
@@ -17,7 +21,6 @@ import java.util.Random;
  */
 public class playerControladora {
     //Variables Generales
-    //String ubicacionCarpeta = "C:\\Users\\MT\\IdeaProjects\\musicPLayer\\src\\main\\resources\\Musica\\";
     URL urlArchivo = playerControladora.class.getClassLoader().getResource("Musica/");
     String ubicacionCarpeta = urlArchivo.getPath();
 
@@ -33,13 +36,17 @@ public class playerControladora {
 
 
 
-    //Botones
+    //Botones y Atrivutos
+    @FXML
+    private ListView<String> listviewAlbums;
+    @FXML
+    private ListView<String> listviewCanciones;
+
     @FXML
     void llenarListViewAlbums(ActionEvent event) {
-
         ArrayList<File> archivosLeidos = leerArchivosMP3Carpeta(ubicacionCarpeta);
         for(int i=0;i<archivosLeidos.size();i++){
-            System.out.println(archivosLeidos.get(i).getName());
+            listviewAlbums.setItems(FXCollections.observableArrayList(leerAlbumsCanciones(archivosLeidos)));
         }
         if(archivosLeidos.isEmpty()){
             System.out.println("Carpeta Vacia");
@@ -47,9 +54,34 @@ public class playerControladora {
     }
 
     @FXML
-    void play(ActionEvent event) {
-        mp3(null).play();
+    void llenarListViewCanciones(ActionEvent event) {
+        ArrayList<String> listaCanciones = leerNombresCanciones(leerArchivosMP3Carpeta(ubicacionCarpeta));
+        ObservableList<String> nombreCanciones = FXCollections.observableArrayList(listaCanciones);
+        listviewCanciones.setItems(nombreCanciones);
     }
+
+    @FXML
+    void play(ActionEvent event) {
+        //CHEQUIAR FUNCION OJO!!
+        String cancionSeleccionada = listviewCanciones.getSelectionModel().getSelectedItems().toString();
+        String nuevaCadena = leerNombrePath(leerArchivosMP3Carpeta(ubicacionCarpeta), EliminaCaracteres(cancionSeleccionada,"[]"));
+        if(nuevaCadena!=null){
+            mp.stop();
+            mp3(nuevaCadena).play();
+        }
+        else{
+            mp3(null).play();
+        }
+
+
+
+    }
+
+
+
+
+
+
 
     //Funciones
     public static ArrayList<File> leerArchivosMP3Carpeta(String ubicacionCarpeta){
@@ -65,6 +97,73 @@ public class playerControladora {
             }
         }
         return listaCanciones;
+    }
+
+    public ArrayList<String> leerAlbumsCanciones(ArrayList<File> listaCanciones){
+        //File cancion = songs.get(1);
+        ArrayList<String> listaNombresAlbums=new ArrayList<String>();
+        String albumAnterior = " ";
+        try {
+            for(int i=0; i<listaCanciones.size(); i++ ){
+                FileInputStream file = new FileInputStream(listaCanciones.get(i));
+                int size = (int)listaCanciones.get(i).length();
+                file.skip(size - 128);
+                byte[] last128 = new byte[128];
+                file.read(last128);
+                String id3 = new String(last128);
+                String tag = id3.substring(0, 3);
+                if (tag.equals("TAG")) {
+                    if(!listaNombresAlbums.contains(id3.substring(33,62))){
+                        listaNombresAlbums.add(id3.substring(33,62));
+                    }
+                    //System.out.println("Title: " + id3.substring(3, 32));
+                    //System.out.println("Artist: " + id3.substring(33, 62));
+                    //System.out.println("Album: " + id3.substring(63, 91));
+                    //System.out.println("Year: " + id3.substring(93, 97));
+                } else{
+                    System.out.println("ERROR EN LOS ATRIVUTOS DE LAS CANCION!");
+                }
+                file.close();
+            }
+        } catch (Exception e) {
+            System.out.println("Error - " + e.toString());
+        }
+        return listaNombresAlbums;
+
+    }
+
+    public ArrayList<String> leerNombresCanciones(ArrayList<File> listaCanciones){
+
+        String Album = listviewAlbums.getSelectionModel().getSelectedItem().toString();
+        ArrayList<String> listaNombresCanciones=new ArrayList<String>();
+
+
+        try {
+            for(int i=0; i<listaCanciones.size(); i++ ){
+                FileInputStream file = new FileInputStream(listaCanciones.get(i));
+                int size = (int)listaCanciones.get(i).length();
+                file.skip(size - 128);
+                byte[] last128 = new byte[128];
+                file.read(last128);
+                String id3 = new String(last128);
+                String tag = id3.substring(0, 3);
+
+                if (tag.equals("TAG")) {
+
+                    if(Album.equals(id3.substring(33,62))){
+                        listaNombresCanciones.add(id3.substring(3,32));
+                    }
+                }
+                else{
+                    System.out.println("ERROR EN LOS ATRIVUTOS!");
+                }
+                file.close();
+            }
+        } catch (Exception e) {
+            System.out.println("Error - " + e.toString());
+        }
+        return listaNombresCanciones;
+
     }
 
     public MediaPlayer mp3(String cancionSeleccionada){
@@ -113,6 +212,33 @@ public class playerControladora {
 
         return stringValido;
     }
+
+    public String leerNombrePath(ArrayList<File> listaCanciones, String Cancion) {
+        try {
+            for (int i = 0; i < listaCanciones.size(); i++) {
+                FileInputStream file = new FileInputStream(listaCanciones.get(i));
+                int size = (int) listaCanciones.get(i).length();
+                file.skip(size - 128);
+                byte[] last128 = new byte[128];
+                file.read(last128);
+                String id3 = new String(last128);
+                String tag = id3.substring(0, 3);
+                if (tag.equals("TAG")) {
+                    if (id3.substring(3, 32).equals(listviewCanciones.getSelectionModel().getSelectedItem().toString())) {
+                        return listaCanciones.get(i).getName();
+                    }
+
+                } else {
+                    System.out.println("ERROR EN LOS ATRIVUTOS!");
+                }
+                file.close();
+            }
+        } catch (Exception e) {
+            System.out.println("Error - " + e.toString());
+        }
+        return null;
+    }
+
 
 
 
